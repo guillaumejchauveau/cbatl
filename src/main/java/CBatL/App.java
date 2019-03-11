be9bec545b8891d2c2cf39e826dd171b7471b76f
Crys <crysyroodpro@outlook.com>
2019-03-11 19:01:21 +0100

    BattleShip class created (the main view)

--- src/main/java/CBatL/App.java
+++ src/main/java/CBatL/App.java
@@ -7,6 +7,7 @@ import CBatL.view.graphicalview.views.BShipPanel;
 import CBatL.view.graphicalview.controllers.QuitController;
 import CBatL.view.graphicalview.controllers.ResetController;
 import CBatL.view.graphicalview.controllers.ChangeViewController;
+import CBatL.view.graphicalview.views.BattleShip;
 import CBatL.view.graphicalview.views.HelpView;
 
 import javax.swing.*;
@@ -18,26 +19,7 @@ public class App {
   public static void main(String[] args)
   {
     System.out.println(new App().getGreeting());
-    JFrame frame = new JFrame();
-    frame.setTitle("BattleShip");
-    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-    HelpView help = new HelpView(frame);
-    BShipPanel bShipP = new BShipPanel(frame, 10, 10);
-
-    ResetController reset = new ResetController(bShipP);
-    QuitController quit = new QuitController(frame);
-    ChangeViewController cvcHelp = new ChangeViewController(frame, help, help.getJMenuBar());
-    ChangeViewController cvcBack = new ChangeViewController(frame, bShipP, bShipP.getJMenuBar());
-
-    bShipP.getResetButton().addActionListener(reset);
-    bShipP.getQuitButton().addActionListener(quit);
-    bShipP.getHelpButton().addActionListener(cvcHelp);
-
-    help.getBackButton().addActionListener(cvcBack);
-
-    frame.setContentPane(bShipP);
-    frame.setVisible(true);
+    new BattleShip();
   }
 
   public String getGreeting() {
---
+++ src/main/java/CBatL/view/graphicalview/views/BattleShip.java
@@ -0,0 +1,43 @@
+package CBatL.view.graphicalview.views;
+
+import CBatL.view.graphicalview.views.BShipPanel;
+import CBatL.view.graphicalview.controllers.QuitController;
+import CBatL.view.graphicalview.controllers.ResetController;
+import CBatL.view.graphicalview.controllers.ChangeViewController;
+import CBatL.view.graphicalview.views.HelpView;
+
+import javax.swing.*;
+
+public class BattleShip extends JFrame
+{
+  public BattleShip ()
+  {
+    this.setTitle("BattleShip");
+    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    HelpView help = new HelpView(this);
+    BShipPanel bShipP = new BShipPanel(this, 10, 10);
+
+    ResetController reset = new ResetController(bShipP);
+    QuitController quit = new QuitController(this);
+    ChangeViewController cvcHelp = new ChangeViewController(this, help, help.getJMenuBar());
+    ChangeViewController cvcBack = new ChangeViewController(this, bShipP, bShipP.getJMenuBar());
+
+    bShipP.getResetButton().addActionListener(reset);
+    bShipP.getQuitButton().addActionListener(quit);
+    bShipP.getHelpButton().addActionListener(cvcHelp);
+
+    help.getBackButton().addActionListener(cvcBack);
+
+    this.setContentPane(bShipP);
+    this.setVisible(true);
+  }
+}
+
+
+
+
+
+
+
+
